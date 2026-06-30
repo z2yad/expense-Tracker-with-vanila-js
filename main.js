22c6cdebe37a6d761bf285d195e1002df85d69f6
zeyad salim <ziadsalim121@gmail.com>
2026-06-30 03:02:40 +0300

    feat: add transaction deletion functionality with unique IDs and row rendering

--- main.js
+++ main.js
@@ -5,9 +5,10 @@ const amountInput = document.getElementById('amount');
 const categoryInput = document.getElementById('category');
 const dateInput = document.getElementById('date');
 const btn = document.querySelector('.btn');
+const transactionsBody = document.querySelector('#transactions-body');
 
 let transactions = [];
-
+//add event
 btn.addEventListener('click', function (e) {
     e.preventDefault();
     const title = titleInput.value;
@@ -49,6 +50,7 @@ btn.addEventListener('click', function (e) {
         return;
     }
     const transaction = {
+        id: Date.now(),
         title,
         amount: Number(amount),
         category,
@@ -61,16 +63,31 @@ btn.addEventListener('click', function (e) {
     renderTransactions(transaction);
     form.reset();
 });
-//create row in  table
-const transactionsBody = document.querySelector('#transactions-body');
+//create
+
 function renderTransactions(transaction) {
     const row = document.createElement('tr');
+    row.setAttribute('data-id', transaction.id);
     row.innerHTML = `
     <td>${transaction.title}</td>
     <td>${transaction.amount}</td>
     <td>${transaction.category}</td>
     <td>${transaction.date}</td>
-    <td><button>Edit</button> <button>Delete</button></td>
+    <td><button class="edit-btn">Edit</button> <button class="del-btn" data-id="${transaction.id}">Delete</button></td>
 `;
     transactionsBody.appendChild(row);
 }
+//update
+//delete
+transactionsBody.addEventListener('click', function (e) {
+    if (e.target.classList.contains('del-btn')) {
+        const id = Number(e.target.dataset.id);
+
+        transactions = transactions.filter((transaction) => transaction.id !== id);
+
+        transactionsBody.innerHTML = "";
+        transactions.forEach((transaction) => {
+            renderTransactions(transaction);
+        });
+    }
+});
